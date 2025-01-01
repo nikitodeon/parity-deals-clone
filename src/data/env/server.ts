76d7e3f674794ec307facc90e29de4be4a7c52f0
Nikita <ragerage333@gmail.com>
2025-01-01 22:22:30 +0300

    4.58

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -8,7 +8,7 @@ export const env = createEnv({
     CLERK_SECRET_KEY: z.string(),
     CLERK_WEBHOOK_SECRET: z.string(),
     STRIPE_SECRET_KEY: z.string(),
-    // STRIPE_WEBHOOK_SECRET: z.string(),
+    STRIPE_WEBHOOK_SECRET: z.string(),
     STRIPE_BASIC_PLAN_PRICE_ID: z.string(),
     STRIPE_STANDARD_PLAN_PRICE_ID: z.string(),
     STRIPE_PREMIUM_PLAN_PRICE_ID: z.string(),
