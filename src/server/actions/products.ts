97685164fd42ff85d00d274a7108d95505cdb350
Nikita <ragerage333@gmail.com>
2025-01-01 17:13:54 +0300

    4.36

--- src/server/actions/products.ts
+++ src/server/actions/products.ts
@@ -15,19 +15,16 @@ import {
   updateProductCustomization as updateProductCustomizationDb,
 } from "@/server/db/products";
 import { redirect } from "next/navigation";
-import {
-  //  canCreateProduct,
-  canCustomizeBanner,
-} from "../permissions";
+import { canCreateProduct, canCustomizeBanner } from "../permissions";
 
 export async function createProduct(
   unsafeData: z.infer<typeof productDetailsSchema>
 ): Promise<{ error: boolean; message: string } | undefined> {
   const { userId } = await auth();
   const { success, data } = productDetailsSchema.safeParse(unsafeData);
-  //   const canCreate = await canCreateProduct(userId)
+  const canCreate = await canCreateProduct(userId);
 
-  if (!success || userId == null /*|| !canCreate*/) {
+  if (!success || userId == null || !canCreate) {
     return { error: true, message: "There was an error creating your product" };
   }
 
